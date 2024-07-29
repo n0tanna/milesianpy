d9ff7390cc914bb0131ec1cc62dc15ace1985b4a
n0tanna <anna.stgermain885@gmail.com>
2024-07-29 00:33:53 -0500

    added negative addition

--- calculation_parser_tests.py
+++ calculation_parser_tests.py
@@ -22,7 +22,6 @@ def test_check_values_positive_3():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("{(4.23+1)+(3+(2+(4+3)))}+2")
     response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    print(response)
     assert 19.23 == response
 
 
@@ -30,7 +29,6 @@ def test_check_values_positive_4():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("{(4.23+1)+(3+(2+(4+3)))}+((30+2)+6)+2")
     response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    print(response)
     assert 57.230000000000004 == response
 
 
@@ -38,7 +36,6 @@ def test_check_values_positive_5():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("4/2")
     response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    print(response)
     assert 2 == response
 
 
@@ -46,7 +43,6 @@ def test_check_values_positive_6():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("(4/2)+2")
     response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    print(response)
     assert 4.0 == response
 
 
@@ -54,7 +50,6 @@ def test_check_values_positive_7():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("4/2/2")
     response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    print(response)
     assert 1.0 == response
 
 
@@ -62,7 +57,6 @@ def test_check_values_positive_8():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("4^2")
     response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    print(response)
     assert 16.0 == response
 
 
@@ -70,5 +64,48 @@ def test_check_values_positive_9():
     basic_parser_class = calculation_parser.CalculationParser()
     equation = list("10^8")
     response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    print(response)
     assert 100000000.0 == response
+
+
+def test_check_values_positive_10():
+    basic_parser_class = calculation_parser.CalculationParser()
+    equation = list("10-8")
+    response = basic_parser_class.no_variable_basic_calculation(equation, False)
+    assert 2.0 == response
+
+
+def test_check_values_positive_11():
+    basic_parser_class = calculation_parser.CalculationParser()
+    equation = list("10-(-8)")
+    response = basic_parser_class.no_variable_basic_calculation(equation, True)
+    assert 18.0 == response
+
+
+def test_check_values_positive_12():
+    basic_parser_class = calculation_parser.CalculationParser()
+    equation = list("(-10)-(-8)")
+    response = basic_parser_class.no_variable_basic_calculation(equation, True)
+    assert -2.0 == response
+
+
+def test_check_values_positive_13():
+    basic_parser_class = calculation_parser.CalculationParser()
+    equation = list("((-10)*(-8))")
+    response = basic_parser_class.no_variable_basic_calculation(equation, True)
+    assert 80.0 == response
+
+
+def test_check_values_positive_14():
+    basic_parser_class = calculation_parser.CalculationParser()
+    equation = list("10^4")
+    response = basic_parser_class.no_variable_basic_calculation(equation, False)
+    assert 10000.0 == response
+
+
+def test_check_values_positive_15():
+    basic_parser_class = calculation_parser.CalculationParser()
+    equation = list("(12-(-3))+(10^4)")
+    response = basic_parser_class.no_variable_basic_calculation(equation, True)
+    assert 10015.0 == response
+
+
--- parsers/number_parser.py
+++ parsers/number_parser.py
@@ -29,7 +29,28 @@ class NumberParser:
         if valid_num:
             converted_equation.append(float(valid_num))
 
+        converted_equation = NumberParser.is_negative(converted_equation)
+
         return converted_equation
+
+    @classmethod
+    def is_negative(cls, user_input: list):
+        iterator = 0
+
+        while len(user_input) > iterator:
+            if user_input[iterator] == '-':
+                if type(user_input[iterator - 1]) is not float and type(user_input[iterator + 1]) is float:
+                    user_input[iterator + 1] = user_input[iterator + 1] * -1
+                    del user_input[iterator]
+
+            iterator += 1
+
+        return user_input
+
+
+
+
+
 
 
 
