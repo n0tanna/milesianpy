d559ebbf7ca0685da1ca1807b557f134a3e40ebc
n0tanna <anna.stgermain885@gmail.com>
2024-07-30 10:20:35 -0500

    completed conversion to test engine

--- tests/parser_tests/calculation_parser_tests.py
+++ tests/parser_tests/calculation_parser_tests.py
@@ -1,111 +1,20 @@
 import pytest
+import json
 from parsers import calculation_parser
-from parsers import number_parser
+from tests import test_runner
+
+calculation_parser_class = calculation_parser.CalculationParser()
+
+@pytest.fixture
+def setup_data():
+    json_file = open('../test_data/calculation_parser_testing_data.json')
+    json_data = json.load(json_file)
+    yield json_data
+    json_file.close()
 
 
-# check_values - positive tests
-def test_check_values_positive_1():
-    calculation_parser_class = calculation_parser.CalculationParser()
-    equation = list("(4.23+234)")
-    response = calculation_parser_class.no_variable_basic_calculation(equation, True)
-    assert 238.23 == response
+def test_convert_to_nums(setup_data):
+    test_runner.test_runner(setup_data, 'no_variable_basic_calculation', calculation_parser_class.no_variable_basic_calculation)
 
-
-def test_check_values_positive_2():
-    calculation_parser_class = calculation_parser.CalculationParser()
-    equation = list("(((4.23+234)+3)+10)")
-    response = calculation_parser_class.no_variable_basic_calculation(equation, True)
-    assert 251.23 == response
-
-
-def test_check_values_positive_3():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("{(4.23+1)+(3+(2+(4+3)))}+2")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert 19.23 == response
-
-
-def test_check_values_positive_4():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("{(4.23+1)+(3+(2+(4+3)))}+((30+2)+6)+2")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert 57.230000000000004 == response
-
-
-def test_check_values_positive_5():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("4/2")
-    response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    assert 2 == response
-
-
-def test_check_values_positive_6():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("(4/2)+2")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert 4.0 == response
-
-
-def test_check_values_positive_7():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("4/2/2")
-    response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    assert 1.0 == response
-
-
-def test_check_values_positive_8():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("4^2")
-    response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    assert 16.0 == response
-
-
-def test_check_values_positive_9():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("10^8")
-    response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    assert 100000000.0 == response
-
-
-def test_check_values_positive_10():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("10-8")
-    response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    assert 2.0 == response
-
-
-def test_check_values_positive_11():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("10-(-8)")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert 18.0 == response
-
-
-def test_check_values_positive_12():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("(-10)-(-8)")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert -2.0 == response
-
-
-def test_check_values_positive_13():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("((-10)*(-8))")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert 80.0 == response
-
-
-def test_check_values_positive_14():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("10^4")
-    response = basic_parser_class.no_variable_basic_calculation(equation, False)
-    assert 10000.0 == response
-
-
-def test_check_values_positive_15():
-    basic_parser_class = calculation_parser.CalculationParser()
-    equation = list("(12-(-3))+(10^4)")
-    response = basic_parser_class.no_variable_basic_calculation(equation, True)
-    assert 10015.0 == response
 
 
